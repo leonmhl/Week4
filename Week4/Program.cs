98d44e3e53ca3fa8864ca37d8bf4d4f5a872865c
leonmhl <leonmoehl@outlook.de>
2023-12-08 18:12:42 +0200

    Week4

---
+++ Week4/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+Random rnd = new Random();
+int myRandom;
+     
+
+
+for (int i = 0; i < 3; i++) 
+{
+    myRandom = rnd.Next(1, 11);     //Range from 1 to 10
+    Console.WriteLine($"{i+1}. {myRandom}");
+}
+
+
+
+//for (int i = 0; i < 10; i++) //i++ ==> i + 1
+//{
+//    Console.WriteLine($"{i+1}. I will not skip Zoom classes");
+//}
---
+++ Week4SecondPart/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+Random rnd = new Random();
+int cpuRandom = rnd.Next(1, 4);
+int userGuess;
+
+bool loopActive = true;
+
+int numberOfTries = 0;
+
+while (numberOfTries<3)
+{
+    Console.WriteLine("Try to get the cpu number:");
+    userGuess = Convert.ToInt32(Console.ReadLine());
+    numberOfTries++;
+
+    if (userGuess == cpuRandom)
+    {
+        Console.WriteLine("You won!");
+        break;
+    }
+    
+}
+Console.WriteLine($"The number of tries: {numberOfTries}");
+Console.WriteLine("Have a nice day!");
